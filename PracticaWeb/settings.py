405ab3816ccfdef8c8c38bc1fec851d4ae12fb77
Aleix Ràmia <aleixramia@hotmail.com>
2019-05-28 13:01:10 +0200

    static_arasi

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATIC_TMP = os.path.join(BASE_DIR,'static')
+os.makedirs(STATIC_TMP,exist_ok=True)
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -165,8 +166,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
